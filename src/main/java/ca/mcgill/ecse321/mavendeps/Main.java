7ada313875eb13706b8295b39f76ff48e14a9be7
Shane McIntosh <shanemcintosh@acm.org>
2018-09-15 21:29:40 -0400

    Initial commit

---
+++ src/main/java/ca/mcgill/ecse321/mavendeps/Main.java
@@ -0,0 +1,11 @@
+package ca.mcgill.ecse321.mavendeps;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Main {
+  public static void main(String[] args) {
+    final Logger logger = LogManager.getLogger(Main.class.getName());
+    logger.info("Hello, world!");
+  }
+}
